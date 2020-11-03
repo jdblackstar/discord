f34ac27b5375b9726fc5dc068167a57482bb16aa
jdblackstar <josh@blackstar.dev>
2020-11-03 11:09:53 -0500

    convert from client to bot

--- app/bot.py
+++ app/bot.py
@@ -1,63 +1,28 @@
 import os
 import random
-
-import discord
-from discord.ext import commands
 from dotenv import load_dotenv
+
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
 bot = commands.Bot(command_prefix='!')
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
-
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f"Hi {member.name}, please remember to choose a role."
-    )
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    responses = [
-        'f you',
-        'i know you are but what am i',
-        'eazi got a pen blackstar hehe'
-    ]
-
-    if message.content == 'speak':
-        response = random.choice(responses)
-        await message.channel.send(response)
-    elif message.content == 'raise-exception':
-        raise discord.DiscordException
+    print(f'{bot.user.name} has connected to Discord!')
 
 @bot.command(name='speak')
 async def speak(ctx):
     responses = [
-        'hi there',
-        'i\'m a bot',
-        'shadowlands is gonna suck'
+        'response 1',
+        'response 2',
+        'response 3',
+        'response 4'
     ]
 
     response = random.choice(responses)
     await ctx.send(response)
 
-@client.event 
-async def on_error(event, *args, **kwargs):
-    with open('err.log', 'a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
-        else:
-            raise
-
-client.run(TOKEN)
 bot.run(TOKEN)
