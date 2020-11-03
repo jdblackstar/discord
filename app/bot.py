84692d976c33bcec15de585f2de1fca2ec2dcfb6
jdblackstar <josh@blackstar.dev>
2020-11-03 11:55:53 -0500

    add create_channel command and role error

--- app/bot.py
+++ app/bot.py
@@ -34,4 +34,18 @@ async def roll(ctx, num_dice: int, num_sides: int):
     ]
     await ctx.send(', '.join(dice))
 
+@bot.command(name='create_channel')
+@commands.has_role('admin')
+async def create_channel(ctx, channel_name='bot_test'):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
+
 bot.run(TOKEN)
