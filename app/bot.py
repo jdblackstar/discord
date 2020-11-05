22ab466f48bae0225caced0dc6f6dd5ce4530d8e
jdblackstar <josh@blackstar.dev>
2020-11-05 11:20:58 -0500

    language fix

--- app/bot.py
+++ app/bot.py
@@ -68,13 +68,13 @@ class Administrative(commands.Cog):
 
         role = discord.utils.get(ctx.guild.roles, name='muted')
         if discord.utils.get(ctx.guild.roles, name='admin') in member.roles:
-            await ctx.send("Nah, can't mute an admin")
+            await ctx.send("Nah, can't mute an admin.")
         elif discord.utils.get(ctx.guild.roles, name='moderator') in member.roles:
-            await ctx.send("Nah, can't mute a moderator")
+            await ctx.send("Nah, can't mute a moderator.")
         elif discord.utils.get(ctx.guild.roles, name='BSbot') in member.roles:
             await ctx.send("I'm not gonna mute myself...")
         elif discord.utils.get(ctx.guild.roles, name='muted') in member.roles:
-            await ctx.send("They're already muted")
+            await ctx.send(f"{member.name} is already muted.")
         else:
             await member.add_roles(role)
 
