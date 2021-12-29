09062320ed840ec0e2756b311c0e85c14c0e103e
Josh <josh@blackstar.dev>
2021-12-29 11:08:15 -0500

    f-string formatting

--- app/bot.py
+++ app/bot.py
@@ -19,22 +19,6 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-##############################################
-########## BOT EVENTS (DEPRECIATED) ##########
-##############################################
-
-# # bot even to post a successful connection to stdout
-# @bot.event
-# async def on_ready():
-#     print(f'{bot.user.name} has connected to Discord!')
-
-# bot event to let a user know that they don't have the proper role
-# for a specific bot command
-# @bot.event
-# async def on_command_error(ctx, error):
-#     if isinstance(error, commands.errors.CheckFailure):
-#         await ctx.send('You do not have the correct role for this command.')
-
 #########################################################
 ########## ADMINISTRATIVE AND MODERATION TOOLS ##########
 #########################################################
@@ -130,7 +114,7 @@ class Administrative(commands.Cog):
     #     if ctx.message.author.server_permissions.administrator or ctx.message.author.id == '194151340090327041':
     #         role = discord.utils.get(member.server.roles, name='Muted')
     #         await bot.add_roles(member, role)
-    #         embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=0xff00f6)
+    #         embed=discord.Embed(title="User Muted!", description=f"**{member}** was muted by **{ctx.message.author}**!", color=0xff00f6)
     #         await bot.say(embed=embed)
     #     else:
     #         embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
@@ -220,7 +204,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
         self.stream_url = data.get('url')
 
     def __str__(self):
-        return '**{0.title}** by **{0.uploader}**'.format(self)
+        return f'**{self.title}** by **{self.uploader}**'
 
     @classmethod
     async def create_source(cls, ctx: commands.Context, search: str, *, loop: asyncio.BaseEventLoop = None):
@@ -230,7 +214,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
         data = await loop.run_in_executor(None, partial)
 
         if data is None:
-            raise YTDLError('Couldn\'t find anything that matches `{}`'.format(search))
+            raise YTDLError(f'Couldn\'t find anything that matches `{search}`')
 
         if 'entries' not in data:
             process_info = data
@@ -242,14 +226,14 @@ class YTDLSource(discord.PCMVolumeTransformer):
                     break
             
             if process_info is None:
-                raise YTDLError('Couldn\'t find anything that matches `{}`'.format(search))
+                raise YTDLError(f'Couldn\'t find anything that matches `{search}`')
 
         webpage_url = process_info['webpage_url']
         partial = functools.partial(cls.ytdl.extract_info, webpage_url, download=False)
         processed_info = await loop.run_in_executor(None, partial)
 
         if processed_info is None:
-            raise YTDLError('Couldn\'t fetch `{}`'.format(webpage_url))
+            raise YTDLError(f'Couldn\'t fetch `{webpage_url}`')
 
         if 'entries' not in processed_info:
             info = processed_info
@@ -259,7 +243,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
                 try:
                     info = processed_info['entries'].pop(0)
                 except IndexError:
-                    raise YTDLError('Couldn\'t retrieve any matches for `{}`'.format(webpage_url))
+                    raise YTDLError(f'Couldn\'t retrieve any matches for `{webpage_url}`')
 
         return cls(ctx, discord.FFmpegPCMAudio(info['url'], **cls.FFMPEG_OPTIONS), data=info)
 
@@ -271,13 +255,13 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
         duration = []
         if days > 0:
-            duration.append('{} days'.format(days))
+            duration.append(f'{days} days')
         if hours > 0:
-            duration.append('{} hours'.format(hours))
+            duration.append(f'{hours} hours')
         if minutes > 0:
-            duration.append('{} minutes'.format(minutes))
+            duration.append(f'{minutes} minutes')
         if seconds > 0:
-            duration.append('{} seconds'.format(seconds))
+            duration.append(f'{seconds} seconds')
 
         return ', '.join(duration)
 
@@ -290,12 +274,12 @@ class Song:
 
     def create_embed(self):
         embed = (discord.Embed(title='Now playing',
-                               description='```css\n{0.source.title}\n```'.format(self),
+                               description=f'```css\n{self.title}\n```',
                                color=discord.Color.blurple())
                  .add_field(name='Duration', value=self.source.duration)
                  .add_field(name='Requested by', value=self.requester.mention)
-                 .add_field(name='Uploader', value='[{0.source.uploader}]({0.source.uploader_url})'.format(self))
-                 .add_field(name='URL', value='[Click]({0.source.url})'.format(self))
+                 .add_field(name='Uploader', value=f'[{self.uploader}]({self.uploader_url})')
+                 .add_field(name='URL', value=f'[Click]({self.url})')
                  .set_thumbnail(url=self.source.thumbnail))
 
         return embed
@@ -425,7 +409,7 @@ class Music(commands.Cog):
         ctx.voice_state = self.get_voice_state(ctx)
 
     async def cog_command_error(self, ctx: commands.Context, error: commands.CommandError):
-        await ctx.send('An error occurred: {}'.format(str(error)))
+        await ctx.send(f'An error occurred: {str(error)}')
 
     @commands.command(name='join', invoke_without_subcommand=True)
     async def _join(self, ctx: commands.Context):
@@ -480,7 +464,7 @@ class Music(commands.Cog):
             return await ctx.send('Volume must be between 0 and 100')
 
         ctx.voice_state.volume = volume / 100
-        await ctx.send('Volume of the player set to {}%'.format(volume))
+        await ctx.send(f'Volume of the player set to {volume}%')
 
     @commands.command(name='now', aliases=['current', 'playing'])
     async def _now(self, ctx: commands.Context):
@@ -543,7 +527,7 @@ class Music(commands.Cog):
                 await ctx.message.add_reaction('⏭')
                 ctx.voice_state.skip()
             else:
-                await ctx.send('Skip vote added, currently at **{}/3**'.format(total_votes))
+                await ctx.send(f'Skip vote added, currently at **{total_votes}/3**')
 
         else:
             await ctx.send('You have already voted to skip this song.')
@@ -566,10 +550,10 @@ class Music(commands.Cog):
 
         queue = ''
         for i, song in enumerate(ctx.voice_state.songs[start:end], start=start):
-            queue += '`{0}.` [**{1.source.title}**]({1.source.url})\n'.format(i + 1, song)
+            queue += f'`{i + 1}.` [**{song.source.title}**]({song.source.url})\n'
 
-        embed = (discord.Embed(description='**{} tracks:**\n\n{}'.format(len(ctx.voice_state.songs), queue))
-                 .set_footer(text='Viewing page {}/{}'.format(page, pages)))
+        embed = (discord.Embed(description=f'**{len(ctx.voice_state.songs)} tracks:**\n\n{queue}')
+                 .set_footer(text=f'Viewing page {page}/{pages}'))
         await ctx.send(embed=embed)
 
     @commands.command(name='shuffle')
@@ -624,12 +608,12 @@ class Music(commands.Cog):
             try:
                 source = await YTDLSource.create_source(ctx, search, loop=self.bot.loop)
             except YTDLError as e:
-                await ctx.send('An error occurred while processing this request: {}'.format(str(e)))
+                await ctx.send(f'An error occurred while processing this request: {str(e)}')
             else:
                 song = Song(source)
 
                 await ctx.voice_state.songs.put(song)
-                await ctx.send('Enqueued {}'.format(str(source)))
+                await ctx.send(f'Enqueued {str(source)}')
 
     @_join.before_invoke
     @_play.before_invoke
@@ -807,7 +791,7 @@ bot.add_cog(Speak(bot))
 
 @bot.event
 async def on_ready():
-    print('Logged in as:\n{0.user.name}\n{0.user.id}'.format(bot))
+    print(f'Logged in as: {bot.user.name} ({bot.user.id})')
 
 @bot.event
 async def on_command_error(ctx, error):
